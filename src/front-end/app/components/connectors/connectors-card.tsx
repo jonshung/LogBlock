15dfd353a7011065acf853b17c0c89e3dc622695
nhatcuong <cuong1312mn@gmail.com>
2024-12-28 11:59:27 +0700

    Update connectors card

--- src/front-end/app/components/connectors/connectors-card.tsx
+++ src/front-end/app/components/connectors/connectors-card.tsx
@@ -1,17 +1,27 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
 
 export default function ConnectorsCard({ name, image }: { name: string; image: string }) {
+    const [directMessage, setDirectMessage] = useState(false);
+
+    const handleDirectMessage = () => {
+        setDirectMessage(!directMessage);
+    };
+
     return (
-        <Link
-            href="/"
-            className="flex items-center w-full h-[65px] text-black tetx-lg font-medium"
-        >
-            <img
-                src={image}
-                width={50}
-                height={50}
-            />
-            <p className="w-full ml-[10px]">{name}</p>
-        </Link>
+        <>
+            <div
+                onClick={handleDirectMessage}
+                className="flex items-center w-full h-[65px] text-black tetx-lg font-medium hover:cursor-pointer"
+            >
+                <img
+                    src={image}
+                    width={50}
+                    height={50}
+                />
+                <p className="w-full ml-[10px]">{name}</p>
+            </div>
+        </>
     );
 }
